c609ca0558c32a156e37a4d5db1f461e0155d115
Ghassan-majali <ghassan.almajali@yahoo.com>
2021-03-22 19:00:21 +0200

    update css and html v2

--- javascript/amman.js
+++ javascript/amman.js
@@ -1,68 +1,68 @@
-// // datatybes
+// datatybes
 
-// /* wellcome in jordan */ 
+/* wellcome in jordan */ 
 
-// // ask about the age  
+// ask about the age  
 
 
-// var age = prompt("whats your age?");
+var age = prompt("whats your age?");
 
-// function theAge (age) {
+function theAge (age) {
     
-//     console.log(age);
-//     if (age >= 18 && age <= 60) {
-//         alert('welcome to jordan');
-//     } else if (age < 18 || age > 60) {
-//         alert('sorry your not allowed');
-//     } else {
-//         alert('please input a number');
-//     }
-// }
+    console.log(age);
+    if (age >= 18 && age <= 60) {
+        alert('welcome to jordan');
+    } else if (age < 18 || age > 60) {
+        alert('sorry your not allowed');
+    } else {
+        alert('please input a number');
+    }
+}
 
-// theAge(age);
+theAge(age);
 
-// // did u visit jordan 
-
-
-// var jordan= prompt("did u visit jordan?");
-
-// function visitJordan (jordan) {
-
-//     console.log(jordan);
-
-//     if(jordan=='yes'){
-//         document.write ('You are from the family');
-//     }else if(jordan=='no'){
-//         document.write('Your chance is now');
-//     }else{
-//         alert('choose betwen yes or no next time');
-
-// }
-
-// }
-// visitJordan(jordan);
+// did u visit jordan 
 
 
-// // rated the site 
-// // between 1 to 5 
+var jordan= prompt("did u visit jordan?");
+
+function visitJordan (jordan) {
+
+    console.log(jordan);
+
+    if(jordan=='yes'){
+        document.write ("<div id=\"div1\">You are from the family</div>");
+    }else if(jordan=='no'){
+        document.write('<div id=\"div1\">Your chance is now</div>');
+    }else{
+        alert('choose betwen yes or no next time');
+
+}
+
+}
+visitJordan(jordan);
 
 
-// var howIsMySite = prompt('how many star you give me from 1 to 5 ?');
-// var result = '';
-// var star = '';
+// rated the site 
+// between 1 to 5 
 
 
-// star ='<img width ="40px" high ="40" src="pic/star.jpg"/>';
+var howIsMySite = prompt('how many star you give me from 1 to 5 ?');
+var result = '';
+var star = '';
 
-// for (var i = 0; i  < howIsMySite; i++) {
-//     if (howIsMySite<=6){
-//         result += star;
+
+star ='<img id="img2" width ="40px" high ="40" src="pic/star.jpg"/>';
+
+for (var i = 0; i  < howIsMySite; i++) {
+    if (howIsMySite<=6){
+        result += star;
                 
-//     }
-//     else if(howIsMySite>5){alert ('chose between 1 - 5'); howIsMySite = prompt('how many star you give me from 1 to 5 ?');}
-// }
+    }
+    else if(howIsMySite>5){alert ('chose between 1 - 5'); howIsMySite = prompt('how many star you give me from 1 to 5 ?');}
+}
 
-// console.log(result);
+console.log(result);
 
-// document.write(result);
+document.write(result);
 
